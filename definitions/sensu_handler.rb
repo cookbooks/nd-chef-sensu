8736e5ffea673a757b4ab6e3530d0b74833b096e
Cameron Johnston <cameron@needle.com>
2012-07-24 13:05:44 -0600

    committing first pass at a sensu_handler definition
    renaming definitions/handler.rb to definitions/sensu_handler.rba
    
    fixing config syntax in sensu_handler usage example
    
    fixing wrong attribute name in json_file resource
    
    fixing how we generate the name for a handler config file
    (cherry picked from commit a733cd79b74fe8f61c97bcb1e2575f41a368a34f)

---
+++ definitions/sensu_handler.rb
@@ -0,0 +1,67 @@
+# installs a sensu handler in /etc/sensu/handlers
+
+# sensu_handler "hipchat.sh" do
+#   source "https://raw.github.com/sensu/sensu-community-plugins/master/handlers/notification/hipchat.rb"
+#   config({ "hipchat" => { "apikey" => "1234abcdefg1234abcdefg", "room" => "Ops"}})
+# end
+
+# sensu_handler "handler-from-mycookbook.rb" do
+#   source "handler-from-mycookbook.rb.erb"
+#   cookbook "mycookbook"
+#   type :template
+#   variables({"foo" => "bar"})
+# end
+
+# sensu_handler "handler-from-anothercookbook.rb" do
+#   source "handler-from-anothercookbook.rb"
+#   cookbook "anothercookbook"
+#   type :file
+# end
+
+define :sensu_handler, :dir => "/etc/sensu/handlers/", :type => :remote, :source => false, :cookbook => false, :variables => false, :checksum => false, :action => :install, :owner => 'root', :group => 'root', :mode => "0755", :config => {} do
+  case params[:action]
+  when :install
+    case params[:type]
+    when :template
+      template ::File.join(params[:dir],params[:name]) do
+        cookbook params[:cookbook] if params[:cookbook].is_a?(String)
+        source params[:source]
+        owner params[:owner]
+        group params[:group]
+        mode params[:mode]
+        variables params[:variables] if params[:variables]
+      end
+    when :file
+      cookbook_file ::File.join(params[:dir],params[:name]) do
+        cookbook params[:cookbook] if params[:cookbook].is_a?(String)
+        source params[:source]
+        owner params[:owner]
+        group params[:group]
+        mode params[:mode]
+      end
+    when :remote
+      remote_file ::File.join(params[:dir],params[:name]) do
+        owner params[:owner]
+        group params[:group]
+        mode params[:mode]
+        source params[:source]
+        checksum params[:checksum] if params[:checksum]
+        not_if {::File.exists?(::File.join(params[:dir],params[:name]))} unless params[:checksum]
+      end
+    end
+  when :remove
+    file ::File.join(params[:dir],params[:name]) do
+      action :delete
+    end
+  end
+
+  unless params[:config].empty? or params[:action] == :delete
+    json_file ::File.join(params[:dir],::File.basename(params[:name],File.extname(params[:name]))+".json") do
+      content params[:config]
+      owner params[:owner]
+      group params[:group]
+      mode "0600" # we don't want to reuse the mode from the handler since it might allow other users to get our lucky charms
+    end
+  end
+
+end
