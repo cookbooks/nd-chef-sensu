b611f756ce1d518e35362b3de7e136db2e01d362
Cameron Johnston <cameron@needle.com>
2012-07-25 11:58:34 -0600

    changing source urls for system plugins to use needle fork

--- recipes/system_plugins.rb
+++ recipes/system_plugins.rb
@@ -1,12 +1,12 @@
 plugins = {
-	"check-disk.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-disk.rb",
-	"check-load.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-load.rb",
-	"check-mem.sh" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/check-mem.sh",
-	"cpu-metrics.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/cpu-metrics.rb",
-	"disk-metrics.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/disk-metrics.rb",
-	"interface-metrics.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/interface-metrics.rb",
-	"memory-metrics.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/memory-metrics.rb",
-	"vmstat-metrics.rb" => "https://raw.github.com/sensu/sensu-community-plugins/master/plugins/system/vmstat-metrics.rb"
+	"check-disk.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/check-disk.rb",
+	"check-load.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/check-load.rb",
+	"check-mem.sh" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/check-mem.sh",
+	"cpu-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/cpu-metrics.rb",
+	"disk-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/disk-metrics.rb",
+	"interface-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/interface-metrics.rb",
+	"memory-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/memory-metrics.rb",
+	"vmstat-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/system/vmstat-metrics.rb"
 }
 
 plugins.each do |name,src|
