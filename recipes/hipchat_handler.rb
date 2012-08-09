d895d19eef74a1f5410ffb611b0b1ff25c64505b
Ben Belchak <ben@needle.com>
2012-08-09 15:18:14 -0600

    using secrets data bag to configure the handler

--- recipes/hipchat_handler.rb
+++ recipes/hipchat_handler.rb
@@ -1,7 +1,13 @@
 sensu_gem "hipchat"
 
+data_bag_key = Chef::EncryptedDataBagItem.load_secret(node['data_bag_key'])
+secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data_bag_key)
+
+apikey = secrets['hipchat']['token']
+room = "Ops"
+
 sensu_handler "hipchat.rb" do
   source "https://raw.github.com/sensu/sensu-community-plugins/master/handlers/notification/hipchat.rb"
-  handler_config({ "apikey" => "", "room" => "" })
-  handler_settings({ "type" => "", "command" => "" })
+  handler_config({ "apikey" => apikey, "room" => room })
+  handler_settings({ "type" => "pipe", "command" => "/etc/sensu/handlers/hipchat.rb" })
 end
