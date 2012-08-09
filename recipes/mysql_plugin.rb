192d8ac14123a0eafe694991774bd08968971f36
Ben Belchak <ben@needle.com>
2012-08-08 20:27:52 -0600

    moving the mysql stuff to the client config

--- recipes/mysql_plugin.rb
+++ recipes/mysql_plugin.rb
@@ -15,7 +15,7 @@ end
 data_bag_key = Chef::EncryptedDataBagItem.load_secret(node['data_bag_key'])
 secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data_bag_key)
 
-node.sensu.mysql_pass = secrets['database']['haystack']['password']
-node.sensu.mysql_user = secrets['database']['haystack']['username']
-node.sensu.mysql_host = secrets['database']['haystack']['host']
-node.sensu.mysql_instance_name = secrets['database']['haystack']['instance_id']
+node.sensu.client.mysql_pass = secrets['database']['haystack']['password']
+node.sensu.client.mysql_user = secrets['database']['haystack']['username']
+node.sensu.client.mysql_host = secrets['database']['haystack']['host']
+node.sensu.client.mysql_instance_name = secrets['database']['haystack']['instance_id']
