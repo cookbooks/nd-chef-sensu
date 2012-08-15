67c72a729503646508ac05bd6e291539169adb0a
Ben Belchak <ben@needle.com>
2012-08-15 17:06:09 -0600

    changing the URL to our hipchat handler

--- recipes/hipchat_handler.rb
+++ recipes/hipchat_handler.rb
@@ -8,7 +8,7 @@ apikey = secrets['hipchat']['token']
 room = "Ops"
 
 sensu_handler "hipchat.rb" do
-  source "https://raw.github.com/sensu/sensu-community-plugins/master/handlers/notification/hipchat.rb"
+  source "https://raw.github.com/needle/sensu-community-plugins/needle/handlers/notification/hipchat.rb"
   handler_config({ "apikey" => apikey, "room" => room })
   handler_settings({ "type" => "pipe", "command" => "/etc/sensu/handlers/hipchat.rb" })
   owner "sensu"
