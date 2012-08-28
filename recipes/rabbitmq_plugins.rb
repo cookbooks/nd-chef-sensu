efe07f68e9cbcd390ed38652518443abc62939e8
Ben Belchak <ben@needle.com>
2012-08-27 18:40:39 -0600

    adding new rabbitmq-queue-check

--- recipes/rabbitmq_plugins.rb
+++ recipes/rabbitmq_plugins.rb
@@ -1,8 +1,9 @@
 gem_package 'carrot-top'
 
 plugins = {
-	"rabbitmq-alive.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/rabbitmq/rabbitmq-alive.rb",
-	"rabbitmq-queue-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/rabbitmq/rabbitmq-queue-metrics.rb"
+    "rabbitmq-alive.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/rabbitmq/rabbitmq-alive.rb",
+    "rabbitmq-queue-metrics.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/rabbitmq/rabbitmq-queue-metrics.rb",
+    "rabbitmq-queue-check.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/rabbitmq/rabbitmq-queue-check.rb"
 }
 
 plugins.each do |name,src|
