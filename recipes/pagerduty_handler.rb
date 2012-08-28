4f70c1b3758fec05ff9d0758aaa3520523c38c43
Ben Belchak <ben@needle.com>
2012-08-28 15:08:09 -0600

    set up the pagerduty

--- recipes/pagerduty_handler.rb
+++ recipes/pagerduty_handler.rb
@@ -1,7 +1,10 @@
 sensu_gem 'redphone'
 
+data_bag_key = Chef::EncryptedDataBagItem.load_secret(node['data_bag_key'])
+secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data_bag_key)
+
 sensu_handler 'pagerduty.rb' do
-  source 'https://raw.github.com/sensu/sensu-community-plugins/master/handlers/notification/pagerduty.rb'
-  handler_config({"api_key" => ""})
-  handler_settings({ "type" => "", "command" => "" })
+  source 'https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/handlers/notification/pagerduty.rb'
+  handler_config({"api_key" => secrets['pagerduty_api_key']})
+  handler_settings({ "type" => "pipe", "command" => "/etc/sensu/handlers/pagerduty.rb" })
 end
