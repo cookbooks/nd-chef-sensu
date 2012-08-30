791de0616504ca54b7a003acf0fcdc76b9a7416d
Ben Belchak <ben@needle.com>
2012-08-30 12:46:18 -0600

    changing sensu api key for pagerduty to sensu-specific

--- recipes/pagerduty_handler.rb
+++ recipes/pagerduty_handler.rb
@@ -7,7 +7,7 @@ secrets = Chef::EncryptedDataBagItem.load("secrets", node.chef_environment, data
 
 sensu_handler 'pagerduty.rb' do
   source 'https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/handlers/notification/pagerduty.rb'
-  handler_config({"api_key" => secrets['pagerduty_api_key']})
+  handler_config({"api_key" => secrets['sensu']['pagerduty_api_key']})
   handler_settings({ "type" => "pipe", "command" => "/etc/sensu/handlers/pagerduty.rb" })
   owner "sensu"
   group "sensu"
