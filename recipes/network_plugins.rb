01efb895f54b748d7ad91504347abb4a39fb4f6a
David Thornton <david@needle.com>
2012-09-17 16:31:23 -0600

    Fixing missing comma in network_plugins.rb

--- recipes/network_plugins.rb
+++ recipes/network_plugins.rb
@@ -1,6 +1,6 @@
 plugins = {
 	"check-http.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/http/check-http.rb",
-	"check-banner.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/network/check-banner.rb"
+	"check-banner.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/network/check-banner.rb",
 	"check-num-connections.sh" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/network/check-num-connections.sh"
 }
 
