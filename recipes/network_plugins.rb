a2f4f24969de37ff4a3d2fe7ee395067b6e4351c
David Thornton <david@needle.com>
2012-09-17 16:05:38 -0600

    Added check-num-connections.sh plugin to network_plugins recipe

--- recipes/network_plugins.rb
+++ recipes/network_plugins.rb
@@ -1,6 +1,7 @@
 plugins = {
 	"check-http.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/http/check-http.rb",
 	"check-banner.rb" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/network/check-banner.rb"
+	"check-num-connections.sh" => "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/network/check-num-connections.sh"
 }
 
 plugins.each do |name,src|
