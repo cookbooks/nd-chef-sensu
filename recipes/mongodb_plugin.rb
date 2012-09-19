32516e3dea1464b86fb709bbe429d370a1861a78
David Thornton <david@needle.com>
2012-09-19 13:49:52 -0600

    Made change to pull mongodb-metrics.rb from sensu-community-plugins/needle branch instead of master

--- recipes/mongodb_plugin.rb
+++ recipes/mongodb_plugin.rb
@@ -8,7 +8,7 @@ python_pip "pymongo" do
 end
 
 sensu_plugin "mongodb-metrics.rb" do
-  source "https://raw.github.com/needle-cookbooks/sensu-community-plugins/master/plugins/mongodb/mongodb-metrics.rb"
+	source "https://raw.github.com/needle-cookbooks/sensu-community-plugins/needle/plugins/mongodb/mongodb-metrics.rb"
 end
 
 sensu_plugin "check_mongodb.py" do
